9825fa0f22e4b26afc0eface3b2e754dcde0ecb3
ChrisMurphy <monkeeman69@googlemail.com>
2016-02-09 12:20:38 +0000

    Travis chrome issue fix

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 [![Build Status](https://travis-ci.org/ChrisMurphy/ng2-components.svg?branch=master)](https://travis-ci.org/ChrisMurphy/ng2-components)
 
 npm install ng2-components
+
+[comment]: https://www.tomsdev.com/blog/2015/tsd-travis-ci-github-rate-limit-reached/
+[comment]: http://swizec.com/blog/how-to-run-javascript-tests-in-chrome-on-travis/swizec/6647
--- karma.conf.js
+++ karma.conf.js
@@ -63,8 +63,19 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        singleRun: true
+        singleRun: true,
+
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        }
     };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
 
     config.set(configuration);
 }
