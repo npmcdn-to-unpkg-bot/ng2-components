f8b3f0a9b9d783d035d4685c9cd851664d22d7b7
ChrisMurphy <monkeeman69@googlemail.com>
2016-02-10 14:35:30 +0000

    Testing travis deploy again

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,8 @@ ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github
 
 echo "Compiling new static content"  
 mkdir $TEMP_DIRECTORY || exit 1  
-cp -r ./coverage $TEMP_DIRECTORY || exit 1
+cp -r coverage $TEMP_DIRECTORY || exit 1
+cp index.html $TEMP_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"  
 git checkout -B gh-pages || exit 1
