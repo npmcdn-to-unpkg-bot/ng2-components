7857031b2b972c9c0f96ef2148c39a07a3669e08
ChrisMurphy <monkeeman69@googlemail.com>
2016-02-11 12:28:33 +0000

    Format code

--- src/hello-world/hello-world.ts
+++ src/hello-world/hello-world.ts
@@ -7,12 +7,13 @@ import {Component} from 'angular2/core';
             color: blue;
         }
     `],
-    template: `<div>
-                  <h1 (click)="onClick()">{{message}}</h1>
-               </div>`
+    template: `
+        <div>
+            <h1 (click)="onClick()">{{message}}</h1>
+        </div>
+    `
 })
 export class HelloWorld {
-
     message = "Click Me ...";
 
     onClick() {
